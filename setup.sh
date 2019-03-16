de644ccf27b2fa047d23b68aec9c374db5bf6d91
Mack Gray <mackenziegray4@gmail.com>
2019-03-16 15:48:01 -0400

    test setup script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
-main () {
+main() {
     cd ~
 
-    #
+    # setup zshrc
     git clone git@github.com:mackenzie-gray/configs.git .configs
+    cd .configs/zsh-custom
+    ln -s .zshrc ~/.zshrc
 
     # Make directories
-    mkdir code
+    mkdir ~/code
 
     # Install ZSH
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
     # Install Brew
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    # Install some programs I use
+    # brew cask install visual-studio-code
+    # brew cask install sourcetree
 }
 
 main
